29586b3ff033e3d654b231698db14faf23676886
Zhaoguanhua <1594996716@qq.com>
2021-05-13 09:38:36 +0800

    docs: add demo model utils folder

---
+++ segmentation_model/__init__.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : zhaoguanhua
+@Email   : zhaogh@hdsxtech.com
+@Time    : 2021/5/13 9:36
+@File    : __init__.py.py
+@Software: PyCharm
+"""
